ebb626bf49e66400c4638260173389a62f4cd317
Doom <diadia911@ukr.net>
2020-10-25 10:16:36 +0200

    some fix

--- app/src/main/java/com/doomhopes/saveinfoonphone/DatabaseActivity.java
+++ app/src/main/java/com/doomhopes/saveinfoonphone/DatabaseActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.SQLData;
 
 public class DatabaseActivity extends AppCompatActivity {
 
     TextView textViewLog;
+    EditText editText;
     private SQLiteDatabase database; // ресурс базы данных
 
     @Override
@@ -20,6 +24,7 @@ public class DatabaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_database);
 
         textViewLog = findViewById(R.id.db_log_txt);
+        editText = findViewById(R.id.editTextNewSTR);
 
         database = openOrCreateDatabase("storage.db", MODE_PRIVATE,null);
         String query = "CREATE TABLE IF NOT EXISTS Strings ("+
@@ -35,5 +40,15 @@ public class DatabaseActivity extends AppCompatActivity {
             return;
         }
         textViewLog.setText("OK!");
+    }
+
+    public void OnClickAddStr(View view) {
+        String str = String.valueOf(editText.getText());
+        if(str.contentEquals(""))
+        {
+            textViewLog.setText("Please, Enter text");
+           // Toast.makeText(this,"Enter text",Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
