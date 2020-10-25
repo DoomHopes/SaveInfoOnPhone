e0bf80090231601a110e856f3a753ee1390e1a87
Doom <diadia911@ukr.net>
2020-10-25 12:51:05 +0200

    File

--- app/src/main/java/com/doomhopes/saveinfoonphone/FileActivity.java
+++ app/src/main/java/com/doomhopes/saveinfoonphone/FileActivity.java
@@ -8,9 +8,12 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileActivity extends AppCompatActivity {
 
@@ -28,11 +31,19 @@ public class FileActivity extends AppCompatActivity {
         textViewLog = findViewById(R.id.textViewFileLog);
         textViewRes = findViewById(R.id.textViewResFile);
         textViewRes.setMovementMethod( new ScrollingMovementMethod());
+        textViewLog.setMovementMethod( new ScrollingMovementMethod());
 
         editText = findViewById(R.id.editText);
     }
 
     public void OnClickAdd(View view) {
+
+        String str = String.valueOf(editText.getText());
+        if(str.contentEquals("")){
+            Toast.makeText(this,"Please, enter text", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         FileOutputStream f;
         try{
             f = openFileOutput(FILE_NAME, Context.MODE_APPEND);
@@ -40,6 +51,35 @@ public class FileActivity extends AppCompatActivity {
             textViewLog.setText(e.getMessage());
             return;
         }
+
+        byte[] buf = str.getBytes();
+        try{
+            f.write(buf);
+            f.flush();
+            f.close();
+        } catch (IOException e) {
+            textViewLog.setText(e.getMessage());
+            return;
+        }
         textViewLog.setText("Open Ok");
+
+        FileInputStream r;
+        try{
+            r = openFileInput(FILE_NAME);
+        }catch (FileNotFoundException ex){
+            textViewLog.setText(ex.getMessage());
+            return;
+        }
+
+        buf = new byte[1024];
+        try{
+            r.read(buf);
+            r.close();
+        }catch (IOException ex){
+            textViewLog.setText(ex.getMessage());
+            return;
+        }
+        str = new String(buf);
+        textViewRes.setText(str);
     }
 }
